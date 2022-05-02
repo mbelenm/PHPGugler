0d411db3d2d9f4d2d6f0bc6d57f8a4d3d6a177fa
Marcos Santia <maycolsantia@gmai.com>
2022-05-01 21:23:12 -0300

    terminando hasta el punto 3 del tp

--- tp1/unicoScript.php
+++ tp1/unicoScript.php
@@ -43,29 +43,63 @@ function verificarFecha(&$fecha,&$valido){
     }
 }
 
+function calcularEdad(&$fecha){
+    /* Configuro la zona horaria y declaro una variable con la fecha actual */
+    date_default_timezone_set('America/Argentina/Buenos_Aires');    
+    /* Creo una variable a la cual le asigno la fecha con formato de fecha UNIX para poder compararlas */
+    $fechaActual = strtotime($DateAndTime = date('d/m/Y'));
+    $fechaNacimiento = strtotime($fecha);
+    /* Verifico que la fecha de nacimiento no sea mayor a la fecha actual */
+    if($fechaActual > $fechaNacimiento){
+        $fechaValida = true;
+    }else{
+        $fechaValida = false;
+    }
+    /* Calcular edad */
+    if($fechaValida == true){
+        $anioActual = date('d/m/Y');
+
+        $arregloAnioActual = explode("/",$anioActual);
+        $anioActual = $arregloAnioActual[2];/* Aplico exple() para quedarme con el año del string */
+        $anioNacimiento = $fecha;
+        $arregloAnioNacimiento = explode("/",$anioNacimiento);
+        $anioNacimiento = $arregloAnioNacimiento[2];
+        $edad = ($anioActual - $anioNacimiento);
+        return $edad;
+    }else{
+        echo "Fecha no valida";
+    }
+}
+
+
 /* Preguntar porque no funciona solamente pasando la fecha */
 
 /* BODY */
 
-$fecha = "10/10/2022";
-
-verificarFecha($fecha,$valido);
-
-if ($valido == true){
-    echo "fecha valida";
-}else{
-    echo "fecha invalida";
-}
-
+$fecha = "10/04/1995";
 echo "<br/>";
 
+echo calcularEdad($fecha);
 
-anioBisiesto($fecha,$bisiesto);
-if ($bisiesto == true){
-    echo "la fecha ingresada es un año bisesto";
-}else{
-    echo "la fecha ingresada no es un año bisiesto";
-}
+
+// verificarFecha($fecha,$valido);
+
+// if ($valido == true){
+//     echo "fecha valida";
+// }else{
+//     echo "fecha invalida";
+// }
+
+// echo "<br/>";
+
+
+// anioBisiesto($fecha,$bisiesto);
+// if ($bisiesto == true){
+//     echo "la fecha ingresada es un año bisesto";
+// }else{
+//     echo "la fecha ingresada no es un año bisiesto";
+// }
+
 
 
 
