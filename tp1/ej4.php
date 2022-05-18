bb7715d0d45af0d82b3cf64b6307d017e2239bfb
marcosSantia <maycolsantia@gmail.com>
2022-05-18 09:13:29 -0300

    tp1 punto 4

--- tp1/ej4.php
+++ tp1/ej4.php
@@ -7,83 +7,131 @@ ini_set("display_errors", "on");
 /* Función para procesar la información personal de un usuario. */
 $personas = array(
     array(
-        'apellido' => 'Romero', 'nombre' => 'Fernando',
+        'apellido' => 'Romero', 
+        /* clave        valor    */
+        'nombre' => 'Fernando',
         'tipo_documento' => 'dni',
         'numero_documento' => 42601837,
         'fecha_nacimiento' =>
         '13/07/2000'
     ),
     array(
-        'apellido' => 'Febre ', 'nombre' => 'Yolanda Rosa ',
+        'apellido' => 'Febre ', 
+        'nombre' => 'Yolanda Rosa ',
         'tipo_documento' => 'dni',
         'numero_documento' =>
         '10587664',
         'fecha_nacimiento' => '15/11/1948'
     ),
     array(
-        'apellido' => 'Gomez', 'nombre' => 'IRMA',
+        'apellido' => 'Gomez', 
+        'nombre' => 'IRMA',
         'tipo_documento' => 'lc',
         'numero_documento' => '5362812',
         'fecha_nacimiento' => '22/05/1918'
     ),
     array(
-        'apellido' => 'Lissa ', 'nombre' => 'Romina ',
+        'apellido' => 'Lissa ', 
+        'nombre' => 'Romina ',
         'tipo_documento' => 'dni',
         'numero_documento' => '12385035',
         'fecha_nacimiento' => '13/05/1958'
     ),
     array(
-        'apellido' => 'Friz', 'nombre' => 'Carlos Rolando',
+        'apellido' => 'Friz', 
+        'nombre' => 'Carlos Rolando',
         'tipo_documento' => 'dni',
         'numero_documento' => '14128874',
         'fecha_nacimiento' => '01/05/1961'
     ),
     array(
-        'apellido' => 'Garzia', 'nombre' => 'Marta Camila',
+        'apellido' => 'Garzia', 
+        'nombre' => 'Marta Camila',
         'tipo_documento' => 'dni',
         'numero_documento' => '17121841',
         'fecha_nacimiento' => '23/11/1964'
     ),
     array(
-        'apellido' => 'Soñez', 'nombre' => 'Jorge Gonzalo',
+        'apellido' => 'Soñez', 
+        'nombre' => 'Jorge Gonzalo',
         'tipo_documento' => 'dni',
         'numero_documento' => '13695121',
         'fecha_nacimiento' => '09/08/1959'
     ),
     array(
-        'apellido' => 'PEREZ', 'nombre' => 'GISELA YANINA',
+        'apellido' => 'PEREZ', 
+        'nombre' => 'GISELA YANINA',
         'tipo_documento' => 'dni',
         'numero_documento' => '31486546',
         'fecha_nacimiento' => '25/03/1985'
     ),
     array(
-        'apellido' => 'YNZA', 'nombre' => 'CARLA',
+        'apellido' => 'YNZA', 
+        'nombre' => 'CARLA',
         'tipo_documento' => 'dni',
         'numero_documento' => '28302680',
         'fecha_nacimiento' => '30/01/1981'
     ),
     array(
-        'apellido' => 'Martinez', 'nombre' => 'Antonia',
+        'apellido' => 'Martinez', 
+        'nombre' => 'Antonia',
         'tipo_documento' => 'dni',
         'numero_documento' => '38054607',
         'fecha_nacimiento' => '27/04/1994'
     )
 );
 
-function verPersona($personas)
-{
+function verPersona($personas){
     foreach ($personas as $claves => $valores) {
         echo 'La clave del array es ' . $claves . '</br>';
+        
+
     }
     foreach ($valores as $unaClave => $unValor) {
         echo 'La clave es ' . $unaClave . ' y contiene el valor de ' . $unValor . '</br>';
     }
 }
 
-verPersona($personas);
+function procesoInfo(&$personas){
+    foreach ($personas as $persona => $campos) {
+        /* punto a */
+        foreach($campos as $unCampo => $unValor){
+            if ($unCampo == 'apellido') {
+                $unValor = strtolower($unValor);
+                $unValor = trim($unValor);
+                $unValor = ucwords($unValor);
+                echo $unValor.'<br />';
+            }
 
-echo "hola";
+            if ($unCampo == 'nombre') {
+                $unValor = strtolower($unValor);
+                $unValor = trim($unValor);
+                $unValor = ucwords($unValor);
+                echo $unValor.'<br />';
+            }
+        }
+        /* punto a hasta aca */
 
-/**
- * Continuar
- */
+        
+    
+    }
+    
+}
+
+
+/* BODY */
+procesoInfo($personas);
+
+
+
+
+
+
+
+/* 
+*El nombre y apellido no deberán tener espacios en blanco extras ni al principio ni al final, y deberán estar todo en minúsculas excepto la primera letra.
+*La edad se deberá calcular en base a la fecha de nacimiento obtenida. En el caso de no ser válida el valor que se deberá mostrar será dos guiones “--”.
+*El tipo de documento deberá estar todo en mayúsculas.
+*La fecha de nacimiento en el caso de no ser correcta, deberá ser reemplazada por el texto “(Fecha no válida)”. 
+*/
+?>
--- tp1/unicoScript.php
+++ tp1/unicoScript.php
@@ -1,7 +1,118 @@
 <?php
+/* esta linea me muestra los errores en la pantalla */
+ini_set("display_errors", "on");
+
+/* vector de personas con registros*/
+$personas = array(
+    array(
+        'apellido' => 'Romero', 
+        /* clave        valor    */
+        'nombre' => 'Fernando',
+        'tipo_documento' => 'dni',
+        'numero_documento' => 42601837,
+        'fecha_nacimiento' => '13/07/2000'
+    ),
+    array(
+        'apellido' => 'Febre ', 
+        'nombre' => 'Yolanda Rosa ',
+        'tipo_documento' => 'dni',
+        'numero_documento' =>
+        '10587664',
+        'fecha_nacimiento' => '15/11/1948'
+    ),
+    array(
+        'apellido' => 'Gomez', 
+        'nombre' => 'IRMA',
+        'tipo_documento' => 'lc',
+        'numero_documento' => '5362812',
+        'fecha_nacimiento' => '22/05/1918'
+    ),
+    array(
+        'apellido' => 'Lissa ', 
+        'nombre' => 'Romina ',
+        'tipo_documento' => 'dni',
+        'numero_documento' => '12385035',
+        'fecha_nacimiento' => '13/05/1958'
+    ),
+    array(
+        'apellido' => 'Friz', 
+        'nombre' => 'Carlos Rolando',
+        'tipo_documento' => 'dni',
+        'numero_documento' => '14128874',
+        'fecha_nacimiento' => '01/05/1961'
+    ),
+    array(
+        'apellido' => 'Garzia', 
+        'nombre' => 'Marta Camila',
+        'tipo_documento' => 'dni',
+        'numero_documento' => '17121841',
+        'fecha_nacimiento' => '23/11/1964'
+    ),
+    array(
+        'apellido' => 'Soñez', 
+        'nombre' => 'Jorge Gonzalo',
+        'tipo_documento' => 'dni',
+        'numero_documento' => '13695121',
+        'fecha_nacimiento' => '09/08/1959'
+    ),
+    array(
+        'apellido' => 'PEREZ', 
+        'nombre' => 'GISELA YANINA',
+        'tipo_documento' => 'dni',
+        'numero_documento' => '31486546',
+        'fecha_nacimiento' => '25/03/1985'
+    ),
+    array(
+        'apellido' => 'YNZA', 
+        'nombre' => 'CARLA',
+        'tipo_documento' => 'dni',
+        'numero_documento' => '28302680',
+        'fecha_nacimiento' => '30/01/1981'
+    ),
+    array(
+        'apellido' => 'Martinez', 
+        'nombre' => 'Antonia',
+        'tipo_documento' => 'dni',
+        'numero_documento' => '38054607',
+        'fecha_nacimiento' => '27/04/1994'
+    )
+);
+
+function procesoInfo(&$personas){
+    foreach ($personas as $persona => $campos) {
+        /* punto a */
+        foreach($campos as $unCampo => $unValor){
+            if ($unCampo == 'apellido') {
+                $unValor = strtolower($unValor);
+                $unValor = trim($unValor);
+                $unValor = ucwords($unValor);
+                echo $unValor.'<br />';
+            }
+
+            if ($unCampo == 'nombre') {
+                $unValor = strtolower($unValor);
+                $unValor = trim($unValor);
+                $unValor = ucwords($unValor);
+                echo $unValor.'<br />';
+            }
+            /* punto a hasta aca */
+            
+            /* punto b */
+            if ($unCampo == 'fecha_nacimiento') {
+                $fecha = $unValor;
+                echo $fecha;
+                // echo calcularEdad($fecha).'<br />';
+            }
+        }
+    }
+    
+}
+
+
+
+
 
 /* Función para validar si un año es bisiesto. */
-
 function anioBisiesto(&$fecha,&$bisiesto){
     /* Convierto la fecha en variables con las que pueda trabajar */
     $separador = "/";
@@ -51,12 +162,12 @@ function calcularEdad(&$fecha){
     $fechaNacimiento = strtotime($fecha);
     /* Verifico que la fecha de nacimiento no sea mayor a la fecha actual */
     if($fechaActual > $fechaNacimiento){
-        $fechaValida = true;
+        $fechaValor = true;
     }else{
-        $fechaValida = false;
+        $fechaValor = false;
     }
     /* Calcular edad */
-    if($fechaValida == true){
+    if($fechaValor == true){
         $anioActual = date('d/m/Y');
 
         $arregloAnioActual = explode("/",$anioActual);
@@ -72,37 +183,43 @@ function calcularEdad(&$fecha){
 }
 
 
+
+
 /* Preguntar porque no funciona solamente pasando la fecha */
-
 /* BODY */
+procesoInfo($personas);
+
+// $fecha = "10/04/1995";
+
+// echo calcularEdad($fecha);
+
+// echo "<br/>";
 
 
-$fecha = "10/04/1995";
+// verificarFecha($fecha,$valido);
 
-echo calcularEdad($fecha);
+// if ($valido == true){
+//     echo "fecha valida";
+// }else{
+//     echo "fecha invalida";
+// }
 
-echo "<br/>";
-
-
-verificarFecha($fecha,$valido);
-
-if ($valido == true){
-    echo "fecha valida";
-}else{
-    echo "fecha invalida";
-}
-
-echo "<br/>";
+// echo "<br/>";
 
 
-anioBisiesto($fecha,$bisiesto);
-if ($bisiesto == true){
-    echo "la fecha ingresada es un año bisesto";
-}else{
-    echo "la fecha ingresada no es un año bisiesto";
-}
+// anioBisiesto($fecha,$bisiesto);
+// if ($bisiesto == true){
+//     echo "la fecha ingresada es un año bisesto";
+// }else{
+//     echo "la fecha ingresada no es un año bisiesto";
+// }
 
 
-
+/* 
+*El nombre y apellido no deberán tener espacios en blanco extras ni al principio ni al final, y deberán estar todo en minúsculas excepto la primera letra.
+*La edad se deberá calcular en base a la fecha de nacimiento obtenida. En el caso de no ser válida el valor que se deberá mostrar será dos guiones “--”.
+*El tipo de documento deberá estar todo en mayúsculas.
+*La fecha de nacimiento en el caso de no ser correcta, deberá ser reemplazada por el texto “(Fecha no válida)”. 
+*/
 
 ?>
